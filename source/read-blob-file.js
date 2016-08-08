a6bb85fe1e0b8796592f407c057324a7fafb71d4
Sergio Daniel Xalambrí <daniel@platzi.com>
2016-08-07 23:21:48 -0500

    [add] promise wrapped for read blob file as base64

---
+++ source/read-blob-file.js
@@ -0,0 +1,20 @@
+/**
+ * Read Blob file as a base64 string
+ * @param  {Blob}    file The Blob file instance to read
+ * @return {Promise}      A reader promise
+ */
+function readBlobFile(file: Blob): Promise {
+  function promiseHandler(resolve: Function, reject: Function): FileReader {
+    const reader: FileReader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function onReadEnd() {
+      if (reader.error) return reject(reader.error);
+      return resolve(reader.result);
+    }
+    return reader;
+  }
+
+  return new Promise(promiseHandler);
+}
+
+export default readBlobFile;
